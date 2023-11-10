64af89e777460fe7bf912ee79ebd0fa067dd2c35
TarakaKoda <taraka.koda@gmail.com>
2023-11-10 17:05:56 +0530

    Build the color mode  switch

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,13 +1,20 @@
-import { HStack, Image, Text} from "@chakra-ui/react"
-import logo from '../assets/logo.webp';
-
+import { HStack, Image, Text, Show } from "@chakra-ui/react";
+import logo from "../assets/logo.webp";
+import ColorModeSwitch from "./ColorModeSwitch";
 const NavBar = () => {
   return (
-    <HStack>
-        <Image boxSize="60px" src={logo}/>
-        <Text fontFamily='futurama' mixBlendMode='difference' marginLeft='-12px'>NexGen Nexus</Text>
+    <HStack justifyContent="space-between" padding="10px">
+      <HStack marginLeft="-12px">
+        <Show above="lg">
+          <Image boxSize="60px" src={logo} />
+        </Show>
+        <Text fontFamily="futurama" marginLeft="-12px">
+          NexGen Nexus
+        </Text>
+      </HStack>
+      <ColorModeSwitch />
     </HStack>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
