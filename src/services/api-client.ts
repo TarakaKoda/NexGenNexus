82ffd58062b70ea8f99c124547b551c732404c7e
TarakaKoda <taraka.koda@gmail.com>
2023-11-29 11:45:42 +0530

    Refactor- fetchResponse interface.

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -8,9 +8,9 @@ const axiosInstance = axios.create({
 });
 
 export interface FetchResponse<T> {
-  length: number;
+  length?: number;
   count: number;
-  next: string | null;
+  next?: string | null;
   results: T[];
 }
 
