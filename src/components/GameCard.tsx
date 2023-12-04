d1390f1d22c9396ce195f419ba1a4e4ce1792b3f
TarakaKoda <taraka.koda@gmail.com>
2023-12-04 20:16:52 +0530

    Refactor- move entity interfaces

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,5 +1,5 @@
-import { Card, Image, CardBody, Heading, HStack} from "@chakra-ui/react";
-import { Game } from "../hooks/useGames";
+import { Card, Image, CardBody, Heading, HStack } from "@chakra-ui/react";
+import { Game } from "../entities/Game";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
 import getCroppedImageUrl from "../services/image-url";
@@ -22,7 +22,7 @@ const GameCard = ({ game }: Props) => {
           <CriticScore score={game.metacritic} />
         </HStack>
         <Heading fontSize="2xl">
-          <Link to={'games/' + game.slug}>{game.name}</Link>
+          <Link to={"games/" + game.slug}>{game.name}</Link>
           <Emoji rating={game.rating_top} />
         </Heading>
       </CardBody>
